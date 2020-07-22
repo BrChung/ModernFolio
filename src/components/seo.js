c6ffb8c9c5dcfc1fac1ecd52301ebc913fe8a7c3
Brian Chung <35940686+BrChung@users.noreply.github.com>
2020-07-22 13:53:05 -0700

    Revert "⚙️ Defer Non-critical CSS"
    This reverts commit 49a54d61fa85b286cc7d5834ba8f2d4e38d63673.

--- src/components/seo.js
+++ src/components/seo.js
@@ -130,10 +130,8 @@ function SEO({ description, lang, meta, title }) {
       ].concat(meta)}
       link={[
         {
-          rel: `preload`,
+          rel: `stylesheet`,
           href: `https://fonts.googleapis.com/css2?family=Roboto&display=swap`,
-          as: `style`,
-          onload: `this.onload=null; this.rel='stylesheet'; document.body.classList.add('fontLoaded')`,
         },
         {
           rel: `apple-touch-icon`,
